a25303fe9b7439a45b42e67e1dee3811b2b76e69
PeDitX <pedram.ale@me.com>
2025-08-14 15:14:33 +0330

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -75,7 +75,7 @@ module("luci.controller.wrpplus", package.seeall)
 
 function index()
     entry({"admin", "peditxos"}, nil, "PeDitXOS Tools", 40).dependent = false
-    entry({"admin", "peditxos", "wrpplus"}, template("wrpplus/main"), "Warp+", 1).dependent = true
+    entry({"admin", "peditxos", "wrpplus"}, template("wrpplus/main"), "Warp+", 40).dependent = true
     entry({"admin", "peditxos", "wrpplus_api"}, call("api_handler")).leaf = true
 end
 
